83ef5aaa276f4f6cbb20c12a4ec2f935e4126acd
Kennan LeJeune <kennan@case.edu>
2022-01-05 07:23:10 -0500

    feat: add pre-commit hooks to enforce formatting and conventional commits

--- src/lib/components/ui/OnlineIndicator.svelte
+++ src/lib/components/ui/OnlineIndicator.svelte
@@ -5,7 +5,7 @@
 <span>
   <span class="text-lg font-light">{online ? 'Online' : 'Offline'}</span>
   <span
-    class="indicator-item indicator-middle indicator-center badge badge-xs 
+    class="indicator-item indicator-middle indicator-center badge badge-xs
   {online ? 'bg-success' : 'bg-error'}"
   />
 </span>
